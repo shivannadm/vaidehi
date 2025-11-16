0e685cabde6d72ed780b1c8360f0d30c684e8167
shivannadm <shivannadm6@gmail.com>
2025-11-16 13:02:46 +0530

    Initial project setup

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Landing Page Ready</h1>
+    </main>
+  );
+}
