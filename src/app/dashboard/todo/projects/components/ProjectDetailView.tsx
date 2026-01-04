448ef07ceee2a21cd73865146cf47bbc76cc3a99
shivannadm <shivannadm6@gmail.com>
2026-01-04 22:44:13 +0530

    Update ProjectDetailView.tsx

--- src/app/dashboard/todo/projects/components/ProjectDetailView.tsx
+++ src/app/dashboard/todo/projects/components/ProjectDetailView.tsx
@@ -8,10 +8,10 @@ interface ProjectDetailViewProps {
 
 export default function ProjectDetailView({ projectId }: ProjectDetailViewProps) {
   const router = useRouter();
-  
+
   // This component just redirects to the detail page
   // The actual implementation is in [id]/page.tsx
   router.push(`/dashboard/todo/projects/${projectId}`);
-  
+
   return null;
 }
